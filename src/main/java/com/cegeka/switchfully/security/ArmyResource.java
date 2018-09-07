07390f959f03cb6624c1736b78e1acff2fc0cf33
Gi11i4m <gi11i4m@gmail.com>
2018-09-07 12:47:50 +0200

    Operation Baobab

--- src/main/java/com/cegeka/switchfully/security/ArmyResource.java
+++ src/main/java/com/cegeka/switchfully/security/ArmyResource.java
@@ -1,5 +1,6 @@
 package com.cegeka.switchfully.security;
 
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -11,34 +12,39 @@ import static org.springframework.util.MimeTypeUtils.APPLICATION_JSON_VALUE;
 @RequestMapping(path = ArmyResource.ARMY_RESOURCE_PATH)
 public class ArmyResource {
 
-    public static final String ARMY_RESOURCE_PATH = "/armies";
+  public static final String ARMY_RESOURCE_PATH = "/armies";
 
-    @RequestMapping(method = RequestMethod.GET, produces = APPLICATION_JSON_VALUE ,path = "/{country}")
-    public ArmyInfoDto getDeployedArmyInfo(@PathVariable(value = "country") String country){
-        return ArmyInfoDto.armyInfoDto()
-                .withCountry(country)
-                .withNumberOfTroops(2000)
-                .withxCoordinateOfBase(15)
-                .withyCoordinateOfBase(20);
-    }
+  @RequestMapping(method = RequestMethod.GET, produces = APPLICATION_JSON_VALUE, path = "/{country}")
+  @PreAuthorize("hasAnyRole('PRIVATE', 'GENERAL')")
+  public ArmyInfoDto getDeployedArmyInfo(@PathVariable(value = "country") String country){
+    return ArmyInfoDto.armyInfoDto()
+        .withCountry(country)
+        .withNumberOfTroops(2000)
+        .withxCoordinateOfBase(15)
+        .withyCoordinateOfBase(20);
+  }
 
-    @RequestMapping(method = RequestMethod.POST)
-    public void joinArmy(){
-        //TODO
-    }
+  @RequestMapping(method = RequestMethod.POST)
+  @PreAuthorize("hasRole('CIVILIAN')")
+  public void joinArmy(){
+    //TODO
+  }
 
-    @RequestMapping(method = RequestMethod.POST, path = "/promote/{name}")
-    public void promotePrivate(@PathVariable(value = "name") String name){
-        //TODO
-    }
+  @RequestMapping(method = RequestMethod.POST, path = "/promote/{name}")
+  @PreAuthorize("hasRole('HUMAN_RELATIONSHIPS')")
+  public void promotePrivate(@PathVariable(value = "name") String name){
+    //TODO
+  }
 
-    @RequestMapping(method = RequestMethod.POST, path = "/discharge/{name}")
-    public void dischargeSoldier(@PathVariable(value = "name") String name){
-        //TODO
-    }
+  @RequestMapping(method = RequestMethod.POST, path = "/discharge/{name}")
+  @PreAuthorize("hasRole('HUMAN_RELATIONSHIPS')")
+  public void dischargeSoldier(@PathVariable(value = "name") String name){
+    //TODO
+  }
 
-    @RequestMapping(method = RequestMethod.GET, path = "/nuke")
-    public String launchNukes(){
-        return "The world ends. Not with a bang but a whimper";
-    }
+  @RequestMapping(method = RequestMethod.GET, path = "/nuke")
+  @PreAuthorize("hasRole('GENERAL')")
+  public String launchNukes(){
+    return "The world ends. Not with a bang but a whimper";
+  }
 }
--- src/main/java/com/cegeka/switchfully/security/SecurityConfig.java
+++ src/main/java/com/cegeka/switchfully/security/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.cegeka.switchfully.security;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -13,38 +14,42 @@ import org.springframework.security.web.AuthenticationEntryPoint;
 
 @Configuration
 @EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
-    @Autowired
-    private AuthenticationEntryPoint authEntryPoint;
+  @Autowired
+  private AuthenticationEntryPoint authEntryPoint;
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http.csrf().disable()
-                .sessionManagement()
-                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-                .and().httpBasic()
-                .authenticationEntryPoint(authEntryPoint);
-    }
+  @Override
+  protected void configure(HttpSecurity http) throws Exception {
+    http.csrf().disable()
+        .sessionManagement()
+        .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+        .and().httpBasic()
+        .authenticationEntryPoint(authEntryPoint);
+  }
 
-    @Autowired
-    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-        PasswordEncoder encoder =
-                PasswordEncoderFactories.createDelegatingPasswordEncoder();
+  @Autowired
+  public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+    PasswordEncoder encoder =
+        PasswordEncoderFactories.createDelegatingPasswordEncoder();
 
-        auth.inMemoryAuthentication()
-                .passwordEncoder(encoder)
+    auth.inMemoryAuthentication()
+        .passwordEncoder(encoder)
 //                Password = WORST
-                .withUser("ZWANETTA").password(("{MD5}{MAARTEN}a0b5a6ba469e5fc37559be6a2396830d")).roles("CIVILIAN")
-                .and()
+        .withUser("ZWANETTA").password(("{MD5}{MAARTEN}a0b5a6ba469e5fc37559be6a2396830d")).roles("CIVILIAN")
+        .and()
 //                Password = THANKS
-                .withUser("JMILLER").password(("{MD5}{NIELS}d037465e3ea09dd346e1a7271a111c77")).roles("PRIVATE")
-                .and()
+        .withUser("JMILLER").password(("{MD5}{NIELS}d037465e3ea09dd346e1a7271a111c77")).roles("PRIVATE")
+        .and()
 //                Password = SAM
-                .withUser("UNCLE").password(("{MD5}{ANDRE}212acec2581451c651fb0ee15db1ad59")).roles("HUMAN_RELATIONSHIPS")
-                .and()
+        .withUser("UNCLE").password(("{MD5}{ANDRE}212acec2581451c651fb0ee15db1ad59")).roles("HUMAN_RELATIONSHIPS")
+        .and()
 //                PASSWORD = RALLY
-                .withUser("GENNY").password(("{MD5}{FRIENDS4LIFE}1bf699ee7fe9e40b317197702b6dc44f")).roles("GENERAL");
-    }
+        .withUser("GENNY").password(("{MD5}{FRIENDS4LIFE}1bf699ee7fe9e40b317197702b6dc44f")).roles("GENERAL")
+        .and()
+    // PASSWORD = RALLY
+        .withUser("PRIVATE_HUMAN").password("{MD5}{FRIENDS4LIFE}1bf699ee7fe9e40b317197702b6dc44f").roles("PRIVATE", "HUMAN_RELATIONSHIPS");
+  }
 
 }
